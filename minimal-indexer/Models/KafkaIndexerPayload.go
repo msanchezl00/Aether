99da3ca422a586ff8b53b2abdf8efe239dae28c8
Marioxuloh <mariosan119@gmail.com>
2025-10-04 14:20:14 +0200

    add: modelo de datos procesados al HDFS

--- minimal-indexer/Models/KafkaIndexerPayload.go
+++ minimal-indexer/Models/KafkaIndexerPayload.go
@@ -1,4 +1,9 @@
 package models
 
 type KafkaIndexerPayload struct {
+	Domain  string                           `json:"domain"`
+	Path    string                           `json:"path"`
+	Date    string                           `json:"date"`
+	Tags    []string                         `json:"tags"`
+	Content []map[string]map[string][]string `json:"content"`
 }
