6fb165e83f5473adf7d4ffd91b6ffbcf0600c82b
Marioxuloh <mariosan119@gmail.com>
2025-12-13 13:54:55 +0100

    add: se actualiza la lista de crawled domains y se borran los dominios que se han crawleado en mas tiempo del puesto en la configuracion

--- minimal-crawler/modules/config/crawlerConfig.go
+++ minimal-crawler/modules/config/crawlerConfig.go
@@ -1,9 +1,10 @@
 package config
 
 type CrawlerConfig struct {
-	Seeds     []map[string]int
-	Recursive bool
-	Timeout   float32
-	Workers   int32
-	Robots    bool
+	Seeds       []map[string]int
+	Recursive   bool
+	Timeout     float32
+	Workers     int32
+	Robots      bool
+	ExpiryHours int32
 }
