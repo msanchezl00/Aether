71d0fa51c0f2fd1a61680163531d50e4d19e46b2
Marioxuloh <mariosan119@gmail.com>
2026-01-09 13:10:10 +0100

    fix: cleanup repository

--- scripts/ProducerKafkaHDFS.sh
+++
@@ -1,6 +0,0 @@
- 
-# dar permisos de escritura en todo el HDFS (no recomendado para producción)
-hdfs dfs -chmod -R 777 /
-
-# reiniciar el conector para que inserte los nuevos archivos en HDFS, ya que de primeras al no tener permisos no los inserta
-curl -X POST http://localhost:8083/connectors/hdfs-sink/tasks/0/restart
